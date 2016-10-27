42a98302356a095f91cdd276d4e1ebb5f5d00c98
pablobovina <pablox.bovina@intel.com>
2016-10-27 20:07:12 -0300

    adding spech to text and button2

--- app/src/main/java/com/example/pbovinax/ayudamemoria/ListaActivity.java
+++ app/src/main/java/com/example/pbovinax/ayudamemoria/ListaActivity.java
@@ -1,8 +1,11 @@
 package com.example.pbovinax.ayudamemoria;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +21,7 @@ import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,11 +31,15 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 public class ListaActivity extends AppCompatActivity {
 
     public static final String MY_PREFS_NAME = "MyPrefsFile";
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private TextView txtOutput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,6 +104,7 @@ public class ListaActivity extends AppCompatActivity {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
 
+
                 AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ListaActivity.this);
                 // ...Irrelevant code for customizing the buttons and title
                 LayoutInflater inflater = ListaActivity.this.getLayoutInflater();
@@ -103,6 +112,17 @@ public class ListaActivity extends AppCompatActivity {
                 dialogBuilder.setView(dialogView);
                 final AlertDialog alertDialog = dialogBuilder.create();
                 alertDialog.show();
+
+                Button btnMicrophone = (Button) alertDialog.findViewById(R.id.button2);
+                txtOutput = (TextView) alertDialog.findViewById(R.id.editText);
+                btnMicrophone.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startSpeechToText();
+                    }
+                });
+
                 Button btnsubmit = (Button) alertDialog.findViewById(R.id.button);
                 btnsubmit.setOnClickListener(new View.OnClickListener() {
 
@@ -218,5 +238,46 @@ public class ListaActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Start speech to text intent. This opens up Google Speech Recognition API dialog box to listen the speech input.
+     * */
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Callback for speech recognition activity
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+
+        }
     }
 }
